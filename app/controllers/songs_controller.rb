8fff136a16d2b7b304639b658bb951444e78a38e
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-24 22:55:18 +0200

    move search functionality to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,20 @@
 class SongsController < ApplicationController
   def index
-    @songs ||= Song.order(title: :asc)
-  end
-
-  def show
+    @songs ||= Song.includes(:artist, :album).order(title: :asc)
+    if params[:track_search].present?
+      songs = Song.search(params[:track_search])
+      if songs.present?
+        @songs = songs
+      else
+        redirect_to artists_path, notice: "Sorry, we couldn't find anything. Check out one of these?"
+      end
+    elsif params[:artist_search].present?
+      artists = Artist.search(params[:artist_search])
+      if artists.present?
+        @songs = @songs.joins(:artist).where("artists.name iLIKE ?", "%#{params[:artist_search]}%")
+      else
+        redirect_to artists_path, notice: "Sorry, we couldn't find anything. Check out one of these?"
+      end
+    end
   end
 end
